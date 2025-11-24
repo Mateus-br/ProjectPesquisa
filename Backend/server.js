7243b1185b336b4cffa946e92f27945430220fc6
Mateus Claudio Alves da Silva <mateusclaudioalvesdasilva@gmail.com>
2025-11-24 12:36:02 -0300

    port

--- Backend/server.js
+++ Backend/server.js
@@ -33,7 +33,8 @@ const db = mysql.createConnection({
   host: process.env.MYSQLHOST,
   user: process.env.MYSQLUSER,
   password: process.env.MYSQLPASSWORD,
-  database: process.env.MYSQLDATABASE
+  database: process.env.MYSQLDATABASE,
+  port: process.env.MYSQLPORT
  
 });
 
