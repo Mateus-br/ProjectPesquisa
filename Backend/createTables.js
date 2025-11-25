f2bddb87a914cd4f09daf63bf5347986c9a7d04c
Mateus Claudio Alves da Silva <mateusclaudioalvesdasilva@gmail.com>
2025-11-25 11:14:31 -0300

    add1

--- Backend/createTables.js
+++ Backend/createTables.js
@@ -10,7 +10,16 @@ const db = mysql.createConnection({
 });
 
 const sql = `
-DROP TABLE form_anuncio;`;
+CREATE TABLE IF NOT EXISTS form_anuncio (
+    id INT AUTO_INCREMENT PRIMARY KEY,
+    nomeProjeto VARCHAR(255),
+    categoria VARCHAR(255),
+    descricao TEXT,
+    localizacao VARCHAR(255),
+    contato VARCHAR(255),
+    imagemCapa VARCHAR(255),
+    criado_em TIMESTAMP DEFAULT CURRENT_TIMESTAMP
+);`;
 
 db.query(sql, err => {
   if (err) console.log("Erro:", err);
